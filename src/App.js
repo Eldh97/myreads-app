22c527227188dab85d8525e387a803300de8a2bf
Eldh97 <eldh97@outlook.com>
2019-09-18 03:39:42 +0300

    feat: Add react components to the project
    - Refactoring the code by creating reusable component: Bookshelf, Book, Search
    - Using react-router-dom for navigation

--- src/App.js
+++ src/App.js
@@ -1,36 +1,51 @@
 import React from "react";
-import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from "./BooksAPI";
 import "./App.css";
 import Bookshelf from "./Bookshelf";
-import CreateBook from "./CreateBook";
+import { Route, Link } from "react-router-dom";
+import Search from "./Search";
 
 class BooksApp extends React.Component {
-
-  state = {
-    
-  };
-  
-  componentDidMount(){
-    BooksAPI.getAll()
-    .then(books => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allBooks: [],
+      currentlyReadBooks: [],
+      readBooks: [],
+      wantToReadBooks: []
+    };
+  }
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
       console.log(books);
-    })
+    });
   }
   render() {
     return (
       <div className="app">
         <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
-          </div>
           <div className="list-books-content">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
             <div>
-              <Bookshelf />
-              <Bookshelf />
-              <Bookshelf />
-              {/* <CreateBook/> */}
+              <Route
+                exact
+                path="/"
+                render={() => (
+                  <>
+                    <Bookshelf />
+                    <Bookshelf />
+                    <Bookshelf />
+                    <Link to="/search/" className="open-search">
+                      <button>Add a book</button>
+                    </Link>
+                  </>
+                )}
+              />
             </div>
           </div>
+          <Route exact path="/search" component={Search} />
         </div>
       </div>
     );
