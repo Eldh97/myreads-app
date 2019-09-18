5afb6da0846814e642d11ca52851c2e44e4d3f8d
eldh97 <eldh97@outlook.com>
2019-09-18 08:22:59 +0300

    refactor: Update Bookshelf component

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
-import Book from './Book';
+import Book from "./Book";
 
 class Bookshelf extends Component {
   render() {
+    const { books } = this.props;
+
+    const renderedBooks = books.map(book => (
+      <Book key = {book.id} book ={book} />
+    ));
     return (
       <div className="bookshelf">
         <h2 className="bookshelf-title">Currently Reading</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-              <Book/>
-              <Book/>
-              <Book/>
+            {renderedBooks}
 
             {/* Generate the books here: Book  */}
           </ol>
