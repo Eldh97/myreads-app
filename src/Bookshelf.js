9f5e634ea3bb5d09783ceef36054602d56a68459
eldh97 <eldh97@outlook.com>
2019-09-19 15:04:38 +0300

    refactor: Change to functional component

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,26 +1,19 @@
 import React, { Component } from "react";
 import Book from "./Book";
 
-class Bookshelf extends Component {
-  render() {
-    const { books } = this.props;
+const Bookshelf = ({ heading, books, updateBookShelf }) => {
+  const renderedBooks = books.map(book => (
+    <Book key={book.id} book={book} updateBookShelf={updateBookShelf} />
+  ));
 
-    const renderedBooks = books.map(book => (
-      <Book key = {book.id} book ={book} />
-    ));
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">Currently Reading</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {renderedBooks}
-
-            {/* Generate the books here: Book  */}
-          </ol>
-        </div>
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{heading}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">{renderedBooks}</ol>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Bookshelf;
